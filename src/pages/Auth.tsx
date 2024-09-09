5b4fe4fe7915135622545ddc44f5a2a98af343d0
Lana N. <80714766+lanasussman@users.noreply.github.com>
2024-09-08 20:33:45 -0400

    Lana video12 (#9)
    * added axios, Auth services, types, login and registration handler functions, installed redux
    
    * completed the video 12, but the app is throwing an error
    
    * updated the baseURL
    
    ---------
    
    Co-authored-by: lana <lana@lanas-MacBook-Pro.local>

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,9 +1,18 @@
 import { FC, useState } from 'react'
+import { AuthService } from '../services/auth.service'
+import { toast } from 'react-toastify'
+import { setTokenFromLocalStorage } from '../helpers/localstorage.helper'
+import { useAppDispatch } from '../store/hooks'
+import { useNavigate } from 'react-router-dom'
 
 export const Auth: FC = () => {
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-  const [isLogin, setIsLogin] = useState<boolean>(false)
+
+	const [email, setEmail] = useState<string>('')
+	const [password, setPassword] = useState<string>('')
+	const [isLogin, setIsLogin] = useState<boolean>(false)
+	const dispatch = useAppDispatch()
+	const navigate = useNavigate()
+
   return (
     <div className="mt-40 flex flex-col justify-center bg-slate-900 text-white">
       <h1 className="mb-10 text-center text-xl">
@@ -15,15 +24,79 @@ export const Auth: FC = () => {
         <button onClick={() => {console.log(email + password)}} className="btn btn-green mx-auto ">Submit</button>
       </form>
 
-      <div className="flex justify-center mt-5">
-        {isLogin ? (
-          <button
-            onClick={() => { setIsLogin(!isLogin) }} className="text-slate-300 hover:text-white">You don't have an account</button>
-        ) : (
-          <button
-            onClick={() => { setIsLogin(!isLogin) }} className="text-slate-300 hover:text-white">Already have an account?</button>
-        )}
-      </div>
-    </div>
-  ) 
+	const loginHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+		try {
+			e.preventDefault()
+			const data = await AuthService.login({ email, password })
+			if (data) {
+				setTokenFromLocalStorage('token', data.token)
+				dispatch(login(data))
+				toast.success('You are logged in.')
+				navigate('/')
+			}
+		} catch (err: any) {
+			const error = err.response?.data.message
+			toast.error(error.toString())
+		}
+	}
+	const registrationHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+		try {
+			e.preventDefault()
+			const data = await AuthService.registration({ email, password })
+			if (data) {
+				toast.success('Account has been created.')
+			}
+		} catch (err: any) {
+			const error = err.response?.data.message
+			toast.error(error.toString())
+		}
+	}
+	return (
+		<div className="mt-40 flex flex-col justify-center bg-slate-900 text-white">
+			<h1 className="mb-10 text-center text-xl">
+				{isLogin ? 'Login' : 'Registration'}
+			</h1>
+			<form
+				onSubmit={isLogin ? loginHandler : registrationHandler}
+				className="flex w-1/3 flex-col mx-auto gap-5"
+				action="#"
+			>
+				<input
+					type="text"
+					className="input"
+					placeholder="Email"
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<input
+					type="password"
+					className="input"
+					placeholder="Password"
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button className="btn btn-green mx-auto">Submit</button>
+			</form>
+
+			<div className="flex justify-center mt-5">
+				{isLogin ? (
+					<button
+						onClick={() => {
+							setIsLogin(!isLogin)
+						}}
+						className="text-slate-300 hover:text-white"
+					>
+						You don't have an account
+					</button>
+				) : (
+					<button
+						onClick={() => {
+							setIsLogin(!isLogin)
+						}}
+						className="text-slate-300 hover:text-white"
+					>
+						Already have an account?
+					</button>
+				)}
+			</div>
+		</div>
+	)
 }
---
+++ src/types/types.ts
@@ -0,0 +1,21 @@
+export interface IUser {
+  id: number
+  email: string
+  token: string
+}
+
+export interface IUserData {
+  email: string
+  password: string
+}
+export interface IResponseUser {
+	email: string | undefined
+	id: number | undefined
+	createdAt: string | undefined
+	updatedAt: string | undefined
+	password: string | undefined
+}
+export interface IResponseUserData {
+	token: string | undefined
+  user: IResponseUser
+}
