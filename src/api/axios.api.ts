af21f730a59eec93eea6fa13f7a63945d4811e9c
Georgy Baranov <floridavbv@gmail.com>
2024-09-22 20:53:48 -0600

    add env var

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getTokenFromLocalStorage } from "../helpers/localstorage.helper";
 
 export const instance = axios.create({
-	baseURL: 'https://backapp-dhfngacba4gna8de.eastus-01.azurewebsites.net/api',
+	baseURL: process.env.BASE_URL,
 	headers: {
 		Authorization: 'Bearer ' + getTokenFromLocalStorage() || '',
 	},
